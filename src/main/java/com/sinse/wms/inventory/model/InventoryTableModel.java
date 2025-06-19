61930884fe8713f33985502ff3691fb05a354aec
Kim-sohee <qpdlql013@naver.com>
2025-06-19 16:45:42 +0900

    stock를 inventory에 병합

--- src/main/java/com/sinse/wms/inventory/model/InventoryTableModel.java
+++ src/main/java/com/sinse/wms/inventory/model/InventoryTableModel.java
@@ -1,4 +1,4 @@
-package com.sinse.wms.stock.view.model;
+package com.sinse.wms.inventory.model;
 
 import java.util.List;
 
@@ -7,15 +7,16 @@ import javax.swing.table.AbstractTableModel;
 import com.sinse.wms.product.model.Stock;
 import com.sinse.wms.product.repository.StockDAO;
 
-public class StockTableModel extends AbstractTableModel{
+public class InventoryTableModel extends AbstractTableModel{
 	StockDAO stockDAO;
 	List<Stock> list;
-	String[] column = {};
+	List<String> column;
 	
 	//생성자
-	public StockTableModel() {
+	public InventoryTableModel(List<String> column) {
 		stockDAO = new StockDAO();
 		list = stockDAO.selectAll();
+		this.column = column;
 	}
 	
 	//몇개의 행인지 확인
@@ -27,7 +28,7 @@ public class StockTableModel extends AbstractTableModel{
 	//몇개의 컬럼인지 확인
 	@Override
 	public int getColumnCount() {
-		return column.length;
+		return column.size();
 	}
 	
 	//값 지정하기
@@ -35,5 +36,4 @@ public class StockTableModel extends AbstractTableModel{
 	public Object getValueAt(int rowIndex, int columnIndex) {
 		return null;
 	}
-	
 }
--- src/main/java/com/sinse/wms/inventory/view/InventoryStatusPage.java
+++ src/main/java/com/sinse/wms/inventory/view/InventoryStatusPage.java
@@ -4,19 +4,44 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
+import com.sinse.wms.common.Config;
 import com.sinse.wms.common.view.content.BaseContentPage;
 import com.sinse.wms.common.view.content.LabeledComboBox;
 
 public class InventoryStatusPage extends BaseContentPage {
     JPanel p_wrapper;
     LabeledComboBox[] filters;
+    
+    JTable table;
+    JScrollPane scroll;
+    JPanel p_bottom;	//버튼들을 모아둘 패널
+    JButton bt_search;
+    JButton bt_excel;
+    JButton bt_pdf;
 
     public InventoryStatusPage(Color color) {
-        // ... 기본 스타일 설정 ...
-
+    	// ... 기본 스타일 설정 ...
+    	setBackground(color);
+    	
+    	/*---------------------------------------------------------------------------
+    	 * 		생성
+    	 *--------------------------------------------------------------------------- */
         p_wrapper = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 7));
+        table = new JTable();
+        scroll = new JScrollPane(table);
+        p_bottom = new JPanel();
+        bt_search = new JButton("조회하기");
+        bt_excel = new JButton("excel 다운받기");
+        bt_pdf = new JButton("pdf 다운받기");
+        
+        /*---------------------------------------------------------------------------
+    	 * 		스타일 지정
+    	 *--------------------------------------------------------------------------- */
         p_wrapper.setPreferredSize(new Dimension(870, 80));
         p_wrapper.setOpaque(false);
 
@@ -24,7 +49,16 @@ public class InventoryStatusPage extends BaseContentPage {
         Dimension labelSize = new Dimension(80, 30);
         Dimension comboSize = new Dimension(230, 30);
         Dimension comboSize2 = new Dimension(150, 30);
-
+        
+        //테이블, 버튼 레이아웃 사이즈 지정
+        scroll.setPreferredSize(new Dimension(Config.CONTENT_BODY_WIDTH - Config.CONTENT_BODY_BORDER_WIDTH * 2-20, 500));
+        scroll.setBackground(Color.gray);
+        p_bottom.setPreferredSize(new Dimension(Config.CONTENT_BODY_WIDTH - Config.CONTENT_BODY_BORDER_WIDTH * 2-20, 50));
+        p_bottom.setBackground(new Color(0, 0, 0, 0));	//배경 투명
+        
+        /*---------------------------------------------------------------------------
+    	 * 		이벤트 연결
+    	 *--------------------------------------------------------------------------- */
         // 라벨 및 JComboBox 묶음 지정
         filters = new LabeledComboBox[] { // new String[]{} 대신 데이터 배열 넣어주시면 될 것 같습니다.
             new LabeledComboBox("거래처", new String[]{"A사", "B사", "C사"}, labelSize, comboSize),
@@ -35,10 +69,34 @@ public class InventoryStatusPage extends BaseContentPage {
             new LabeledComboBox("진행상태", new String[]{"요청", "대기", "승인", "반려"}, labelSize, comboSize2)
         };
 
+        //조회 버튼 이벤트
+  		bt_search.addActionListener(e->{
+  			System.out.println("조회 버튼 눌렸당");
+  		});
+  		
+  		//엑셀 버튼 이벤트
+  		bt_excel.addActionListener(e->{
+  			System.out.println("엑셀 버튼 눌렸당");
+  		});
+  		
+  		//pdf 버튼 이벤트
+  		bt_pdf.addActionListener(e->{
+  			System.out.println("pdf 버튼 눌렸당");
+  		});
+  		
+        
+  		/*---------------------------------------------------------------------------
+    	 * 		부착
+    	 *--------------------------------------------------------------------------- */
         for (LabeledComboBox filter : filters) {
             p_wrapper.add(filter);
         }
 
         add(p_wrapper);
+        add(scroll);
+        p_bottom.add(bt_search);
+        p_bottom.add(bt_excel);
+        p_bottom.add(bt_pdf);
+        add(p_bottom);
     }
 }
